937d915da15a592f5f1fe2cf6d8c51ee8c082577
Jorge Kamezawa <jor_kamezawa@hotmail.com>
2021-08-18 02:39:25 -0300

    Alterando os scripts para build

---
+++ scripts/server_prepare.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo rm /home/ec2-user/api-server
+sudo mkdir /home/ec2-user/api-server
+chmod +x /home/ec2-user/api-server
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-cd /home/ec2-user
-sudo java -jar aluraflix-0.0.1-SNAPSHOT.jar
+cd /home/ec2-user/api-server
+sudo java -jar *.jar
